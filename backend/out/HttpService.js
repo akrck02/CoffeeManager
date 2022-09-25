1f20f3e22a1f8db448b89ee4c5b8da5f2fc00c29
akrck02 <43274508+akrck02@users.noreply.github.com>
2022-09-25 20:02:39 +0200

    [clean] Bye Javascript, never again.

--- backend/out/HttpService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Api_1 = require("./Api");
-async function startHttp() {
-    const api = new Api_1.Api();
-    api.start();
-}
-startHttp();
